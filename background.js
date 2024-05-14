82e7d7efbfb5428e87e2ca1cef2269d828be224f
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2024-05-14 05:46:20 -0400

    feature: configure manifest.json and add bookmark button on youtube player

---
+++ background.js
@@ -0,0 +1,26 @@
+let currentTabId = null;
+let currentTabUrl = null;
+let currentVideoId = null;
+console.log("background.js loaded");
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if(changeInfo.status==="complete")
+  try {
+    if (tab.url && tab.url.includes("youtube.com/watch?v=")) {
+      currentTabId = tabId;
+      currentTabUrl = tab.url;
+      currentVideoId = tab.url.split("v=")[1];
+      (async () => {
+        await chrome.tabs.sendMessage(currentTabId, {
+          action: "addBookmarkButton",
+        });
+        
+      })();
+    } else {
+      currentTabId = null;
+      currentTabUrl = null;
+      console.log("Not a youtube video page");
+    }
+  } catch (error) {
+    console.log("Error in background.js: ", error);
+  }
+});
---
+++ content-script.js
@@ -0,0 +1,33 @@
+console.log('content-script.js loaded');
+;(()=>{
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
+    if(message.action === "addBookmarkButton"){
+      setTimeout(() => {
+        addBookmarkButton();
+      }, 2000);
+    }
+  });
+})();
+
+function addBookmarkButton(){
+  const rightControls = document.getElementsByClassName('ytp-right-controls')[0];
+  if(rightControls.childNodes[0].id === "bookmarkButton"){
+    return;
+  }
+  const bookmarkButton = document.createElement('img');
+  bookmarkButton.id = "bookmarkButton";
+  bookmarkButton.src = chrome.runtime.getURL('assets/bookmark.png');
+  bookmarkButton.alt = "Bookmark";
+  const bookmarkButtonStyle = {
+    width: "30px",
+    height: "30px",
+    cursor: "pointer",
+    border: "2px solid #78b0fc",
+    margin: "0 18px 8px",
+    "border-radius": "50%",
+  }
+  Object.assign(bookmarkButton.style, bookmarkButtonStyle);
+  bookmarkButton.title = 'Bookmark this video';
+  rightControls.prepend(bookmarkButton);
+  return;
+}
