7f37e00b9c47c8cd95df0f425295d988d6634da4
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2024-05-14 06:35:54 -0400

    format files using prettier

--- content-script.js
+++ content-script.js
@@ -1,7 +1,7 @@
-console.log('content-script.js loaded');
-;(()=>{
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
-    if(message.action === "addBookmarkButton"){
+console.log("content-script.js loaded");
+(() => {
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "addBookmarkButton") {
       setTimeout(() => {
         addBookmarkButton();
       }, 2000);
@@ -9,14 +9,16 @@ console.log('content-script.js loaded');
   });
 })();
 
-function addBookmarkButton(){
-  const rightControls = document.getElementsByClassName('ytp-right-controls')[0];
-  if(rightControls.childNodes[0].id === "bookmarkButton"){
+function addBookmarkButton() {
+  console.log("Adding bookmark button");
+  const rightControls =
+    document.getElementsByClassName("ytp-right-controls")[0];
+  if (rightControls.childNodes[0].id === "bookmarkButton") {
     return;
   }
-  const bookmarkButton = document.createElement('img');
+  const bookmarkButton = document.createElement("img");
   bookmarkButton.id = "bookmarkButton";
-  bookmarkButton.src = chrome.runtime.getURL('assets/bookmark.png');
+  bookmarkButton.src = chrome.runtime.getURL("assets/bookmark.png");
   bookmarkButton.alt = "Bookmark";
   const bookmarkButtonStyle = {
     width: "30px",
@@ -25,9 +27,9 @@ function addBookmarkButton(){
     border: "2px solid #78b0fc",
     margin: "0 18px 8px",
     "border-radius": "50%",
-  }
+  };
   Object.assign(bookmarkButton.style, bookmarkButtonStyle);
-  bookmarkButton.title = 'Bookmark this video';
+  bookmarkButton.title = "Bookmark this video";
   rightControls.prepend(bookmarkButton);
   return;
 }
